e8d45d13b4b8db5e699136f221a058078f10c801
Máté Kis <40013370+SonOf1998@users.noreply.github.com>
2024-09-15 21:44:08 +0200

    adjusting backend resource

--- azure/infra/backend.tf
+++ azure/infra/backend.tf
@@ -1,8 +1,13 @@
 terraform {
   backend "azurerm" {
-    resource_group_name  = "Databricks-sandbox" 
-    storage_account_name = "databrickssandboxtfstate"                     
-    container_name       = "infra-tfstate"                      
-    key                  = "sandbox.terraform.tfstate"       
+    resource_group_name  = "Databricks-sandbox"
+    storage_account_name = "databrickssandboxtfstate"
+    container_name       = "infra-tfstate"
+    key                  = "sandbox.terraform.tfstate"
+
+    subscription_id = var.subscription_id
+    client_id       = var.application_client_id
+    client_secret   = var.client_secret
+    tenant_id       = var.directory_tenant_id
   }
 }

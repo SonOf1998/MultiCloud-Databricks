bd9e3a9525208ea5236eabbcc7d9482a3dc0e741
Máté Kis <40013370+SonOf1998@users.noreply.github.com>
2024-09-15 22:39:41 +0200

    info

--- azure/infra/backend.tf
+++ azure/infra/backend.tf
@@ -4,5 +4,7 @@ terraform {
     storage_account_name = "databrickssandboxtfstate"
     container_name       = "infra-tfstate"
     key                  = "sandbox.terraform.tfstate"
+
+    # When a terraform command stuck use 'Break lease' on the state file
   }
 }

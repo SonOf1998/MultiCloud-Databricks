4c91e682a9c711cfc8bff7f50f5443159826b752
Máté Kis <40013370+SonOf1998@users.noreply.github.com>
2024-09-16 16:22:43 +0200

    mws permission assignment correction

--- azure/infra/databricks_workspace.tf
+++ azure/infra/databricks_workspace.tf
@@ -8,5 +8,5 @@ resource "azurerm_databricks_workspace" "databricks_sandbox_workspace" {
 resource "databricks_mws_permission_assignment" "sandbox_admin_account_group_workspace_assignment" {
   workspace_id = azurerm_databricks_workspace.databricks_sandbox_workspace.workspace_id
   principal_id = data.databricks_group.sandbox_admin_account_group.id
-  permissions  = "ADMIN"
+  permissions  = ["ADMIN"]
 }

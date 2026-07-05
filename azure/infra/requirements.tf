a98637847cd4a02398bf94ada3fb05887a4bce1c
Máté Kis <ghzman029@gmail.com>
2026-07-05 21:34:02 +0200

    removed required_version

--- azure/infra/requirements.tf
+++ azure/infra/requirements.tf
@@ -1,15 +1,3 @@
 terraform {
-  required_version = "1.9.5"
-
-  required_providers {
-    aws = {
-      source  = "hashicorp/aws"
-      version = "~> 5.67"
-    }
-
-    databricks = {
-      source  = "databricks/databricks"
-      version = "~> 1.51"
-    }
-  }
+  # version is provided in the GitHub action
 }

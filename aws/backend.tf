044d0b6b6f76d6d6a42741866b18630ff224be63
Máté Kis <ghzman029@gmail.com>
2026-07-09 00:11:52 +0200

    region fixed in S3 backend

--- aws/backend.tf
+++ aws/backend.tf
@@ -2,7 +2,7 @@ terraform {
   backend "s3" {
     bucket         = "mkisdbxtfstate"
     key            = "sandbox/terraform.tfstate"
-    region         = local.region
+    region         = "eu-west-1"
     dynamodb_table = "mkisdbxtfstate-locks" # LockID as partition key
   }
 }
